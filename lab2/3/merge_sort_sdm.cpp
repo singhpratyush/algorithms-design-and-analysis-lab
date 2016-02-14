f3944d2e860b2a081bc9f95eac00028f1fea06ec
Pratyush Singh <singh.pratyush96@gmail.com>
2016-02-14 15:10:40 +0530

    Completed "divide-into-maximal-monotonic-increasing-subarray and merge".

--- lab2/3/merge_sort_sdm.cpp
+++ lab2/3/merge_sort_sdm.cpp
@@ -1,11 +1,48 @@
 #include <iostream>
 using namespace std;
 
-class group
+void merge(int *arr, int l, int mid, int r)
 {
-public:
-	int startIndex, endIndex;
-};
+	int i;
+
+	int left_size = mid - l + 1;
+	int right_size = r - mid;
+
+	int left[left_size], right[right_size];
+
+	for(i = l ; i <= mid ; i ++ )
+		left[i - l] = arr[i];
+
+	for(i = mid+1 ; i <= r ; i ++ )
+		right[i - mid - 1] = arr[i];
+
+	int m = 0 , n = 0 ;
+
+	for(i = l ; m < left_size && n < right_size ; i ++ )
+		if (left[m] > right[n])
+			arr[i] = right[n++];
+		else
+			arr[i] = left[m++];
+
+	while( m < left_size)
+		arr[i++] = left[m++];
+
+	while( n < right_size)
+		arr[i++] = right[n++];
+}
+
+void merge_sort_sdm(int *arr, int length, int *grpArr, int grpLength)
+{
+	if(grpLength == 0)
+		return;
+	int start = 0;
+	for(int i = 0 ; i < grpLength ; i ++)
+	{
+		int mid = grpArr[i];
+		int end = grpArr[i+1];
+		merge(arr, start, mid, end);
+	}
+}
 
 int main(void)
 {
@@ -13,11 +50,18 @@ int main(void)
 	cin >> size;
 	int arr[size];
 
+	int groupArr[size], grpPos = 0;
+
 	for(int i = 0 ; i < size ; i ++ )
 		cin >> arr[i];
 
+	for(int i = 0 ; i < size-1 ; i ++ )
+		if(arr[i] > arr[i+1])
+			groupArr[grpPos++] = i;
 
-	merge_sort_sdm(arr, 0, size-1);
+	groupArr[grpPos] = size-1;
+
+	merge_sort_sdm(arr, size, groupArr, grpPos);
 
 	for(int i = 0 ; i < size ; i ++ )
 		cout << arr[i] << " " ;
